c7eac27940b2ed3bff8201e3973dede2ebfca5a3
Jesper <jeppenp12@gmail.com>
2020-03-02 14:42:25 +0100

    Interactivity Button

--- sketch_jan22a/sketch_jan22a.ino
+++ sketch_jan22a/sketch_jan22a.ino
@@ -4,11 +4,13 @@
 
 #define FIREBASE_HOST "smarthome-bc05a.firebaseio.com"                         // the project name address from firebase id
 #define FIREBASE_AUTH "Vwj3ODFO80GDFt0KHdIwRcvnbC7C4b7BQMv8LCqv"                  // the secret key generated from firebase
-#define WIFI_SSID "Jesper P"                                         // input your home or public wifi name 
-#define WIFI_PASSWORD "metoo123"                                  //password of wifi ssid
+#define WIFI_SSID "EASJ-FABLAB"                                         // input your home or public wifi name 
+#define WIFI_PASSWORD "fablabpass!"                                  //password of wifi ssid
 
 String fireStatus = "";                                                     // led status received from firebase
-int led = 5;                   
+int led = 5;     
+int button = 16;
+int temp = 0;              
 void setup() {
   Serial.begin(9600);
   delay(1000);
@@ -31,20 +33,30 @@ void setup() {
 }
 
 void loop() {
+  // Variables
+  temp = digitalRead(button);
+  // Actions
   if(WiFi.status() == WL_CONNECTED) {
     fireStatus = Firebase.getString("devices/master/led_status");
     if (fireStatus == "TRUE") {                                                          // compare the input of led status received from firebase
-      Serial.println("Led Turned ON");                         
+      // Serial.println("Led Turned ON");                         
       digitalWrite(LED_BUILTIN, LOW);                                        //send initial string of led status
       digitalWrite(led, HIGH);
     } 
     else if (fireStatus == "FALSE") {                                                  // compare the input of led status received from firebase
-      Serial.println("Led Turned OFF");
+      // Serial.println("Led Turned OFF");
       digitalWrite(LED_BUILTIN, HIGH);                                               // make bultin led OFF
       digitalWrite(led, LOW);                                                         // make external led OFF
     }
     else {
       Serial.println("Wrong Credential! Please send ON/OFF");
+    }
+    // Button Override
+    if (temp == HIGH) {
+        Firebase.setString("devices/master/interactive", "TRUE");    
+    }
+    else {
+        Firebase.setString("devices/master/interactive", "FALSE");
     }
   }
   else {
