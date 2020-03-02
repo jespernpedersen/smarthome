21a59d3670d85872f5b62a550c9a1ca8e7b67ac4
Jesper <jeppenp12@gmail.com>
2020-03-02 12:55:36 +0100

    Change SHA1 Fingerprint to Google's newest + check if Firebase is on

--- sketch_jan22a/sketch_jan22a.ino
+++ sketch_jan22a/sketch_jan22a.ino
@@ -8,7 +8,7 @@
 #define WIFI_PASSWORD "metoo123"                                  //password of wifi ssid
 
 String fireStatus = "";                                                     // led status received from firebase
-int led = 5;                                                                // for external led
+int led = 5;                   
 void setup() {
   Serial.begin(9600);
   delay(1000);
@@ -27,22 +27,27 @@ void setup() {
   Serial.print("IP Address is : ");
   Serial.println(WiFi.localIP());                                                      //print local IP address
   Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       // connect to firebase
-  Firebase.setString("LED_STATUS", "OFF");                                          //send initial string of led status
+  Firebase.setString("devices/master/led_status", "FALSE");                                          //send initial string of led status
 }
 
 void loop() {
-  fireStatus = Firebase.getString("LED_STATUS");                                      // get ld status input from firebase
-  if (fireStatus == "ON") {                                                          // compare the input of led status received from firebase
-    Serial.println("Led Turned ON");                         
-    digitalWrite(LED_BUILTIN, LOW);                                                  // make bultin led ON
-    digitalWrite(led, HIGH);                                                         // make external led ON
-  } 
-  else if (fireStatus == "OFF") {                                                  // compare the input of led status received from firebase
-    Serial.println("Led Turned OFF");
-    digitalWrite(LED_BUILTIN, HIGH);                                               // make bultin led OFF
-    digitalWrite(led, LOW);                                                         // make external led OFF
+  if(WiFi.status() == WL_CONNECTED) {
+    fireStatus = Firebase.getString("devices/master/led_status");
+    if (fireStatus == "TRUE") {                                                          // compare the input of led status received from firebase
+      Serial.println("Led Turned ON");                         
+      digitalWrite(LED_BUILTIN, LOW);                                        //send initial string of led status
+      digitalWrite(led, HIGH);
+    } 
+    else if (fireStatus == "FALSE") {                                                  // compare the input of led status received from firebase
+      Serial.println("Led Turned OFF");
+      digitalWrite(LED_BUILTIN, HIGH);                                               // make bultin led OFF
+      digitalWrite(led, LOW);                                                         // make external led OFF
+    }
+    else {
+      Serial.println("Wrong Credential! Please send ON/OFF");
+    }
   }
   else {
-    Serial.println("Wrong Credential! Please send ON/OFF");
+      Serial.println("Could not connect to internet");    
   }
 }
